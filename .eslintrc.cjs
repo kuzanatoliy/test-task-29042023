d94879996473f157e94967995465c7e9e81f920b
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-04-29 15:57:24 +0300

    chore: set up prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -11,4 +12,12 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      rules: {
+        'no-undef': 0,
+      },
+    },
+  ],
+};
