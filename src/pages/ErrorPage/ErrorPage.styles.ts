7efbc848270590c8c34c13afb221fbff9b61e808
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-04-30 01:37:42 +0300

    feat: error page

---
+++ src/pages/ErrorPage/ErrorPage.styles.ts
@@ -0,0 +1,10 @@
+import { SxProps } from '@mui/material';
+
+export const container: SxProps = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: 4,
+};
--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,1 +1,26 @@
-export const ErrorPage = () => <div>Error Page</div>;
+import { useNavigate } from 'react-router-dom';
+import { Button, Container, Typography } from '@mui/material';
+import { ArrowForward } from '@mui/icons-material';
+
+import * as styles from './ErrorPage.styles';
+
+export const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <Container sx={styles.container}>
+      <Typography variant='h2' component='h2'>
+        404 - Page not found
+      </Typography>
+      <Typography variant='body1'>
+        The page cannot be reached. Check if it exists
+      </Typography>
+      <Button
+        variant='contained'
+        onClick={() => navigate('/home')}
+        endIcon={<ArrowForward />}
+      >
+        Go to home
+      </Button>
+    </Container>
+  );
+};
