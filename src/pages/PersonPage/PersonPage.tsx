f6f796b6e499662d7d6362e9b063d639706cd5eb
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-05-01 23:21:53 +0300

    feat: person page

--- src/pages/PersonPage/PersonPage.tsx
+++ src/pages/PersonPage/PersonPage.tsx
@@ -1,1 +1,74 @@
-export const PersonPage = () => <div>Person Page</div>;
+import { useEffect } from 'react';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import {
+  fetchPerson,
+  selectPersonStatus,
+  selectPerson,
+  useDispatch,
+  useSelector,
+} from '../../store';
+
+import * as styles from './PersonPage.styles';
+import { ERequestStatus } from '../../types';
+
+export const PersonPage = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const status = useSelector(selectPersonStatus);
+  const person = useSelector(selectPerson);
+
+  if (status === ERequestStatus.ERROR) {
+    navigate('/error');
+  }
+
+  useEffect(() => {
+    dispatch(fetchPerson({ id }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Container sx={styles.container}>
+      <Box sx={styles.content}>
+        {status === ERequestStatus.LOADING ? (
+          <Typography variant='body1'>Loading...</Typography>
+        ) : (
+          <>
+            <Typography variant='h2'>{person?.name}</Typography>
+            <Box>
+              <Typography variant='body1' component='span'>
+                {'Birth year: '}
+              </Typography>
+              <Typography variant='body1' component='span'>
+                {person?.birth_year}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant='body1' component='span'>
+                {'Eye color: '}
+              </Typography>
+              <Typography variant='body1' component='span'>
+                {person?.eye_color}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant='body1' component='span'>
+                {'Gender: '}
+              </Typography>
+              <Typography variant='body1' component='span'>
+                {person?.gender}
+              </Typography>
+            </Box>
+          </>
+        )}
+      </Box>
+      <Box sx={styles.nav}>
+        <Button variant='contained' onClick={() => navigate('/home')}>
+          Back
+        </Button>
+      </Box>
+    </Container>
+  );
+};
